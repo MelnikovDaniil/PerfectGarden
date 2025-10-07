b171df5e19555ea866d399ec7a8cfe073016ce27
MelnikovDaniil <3148dan@gmail.com>
2025-10-08 01:02:02 +0300

    Character and dialogue added

---
+++ Assets/Scripts/DTO/OrderCharacterInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterInfo", menuName = "ScriptableObjects/OrderCharacters")]
+public class OrderCharacterInfo : ScriptableObject
+{
+    public string name;
+    public Sprite regular;
+    public Sprite angry;
+    public Sprite fun;
+    public Sprite qute;
+}
